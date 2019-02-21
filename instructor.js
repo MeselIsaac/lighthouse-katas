31fc8014f6554510349b95f45f02609dc874b1f9
Mesel <meselisaac03@hotmail.com>
2019-02-21 18:51:09 +0000

    Completed Kata 4 'instructorWithLongestName' function

---
+++ instructor.js
@@ -0,0 +1,34 @@
+const instructorWithLongestName = function(instructors) {
+  var count = 0
+  var longestName = ""
+  for (var i = 0; i < instructors.length; i++) {
+    if (instructors[i].name.length > longestName.length) {
+      count = i;
+      longestName = instructors[i].name;
+    }
+  }
+  return instructors[count];
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
+
+/*//var person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+
+var instructors = {
+  name: "Samuel", ""
+}*/
